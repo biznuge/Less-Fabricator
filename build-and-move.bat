87193d7e8e7935a73e7fcff06edaed0f546ed00d
biznuge <graham.theguerrilla@googlemail.com>
2017-09-01 23:46:42 +0100

    added css.map to build-and-move.bat

--- build-and-move.bat
+++ build-and-move.bat
@@ -1,4 +1,6 @@
 cp build/toolkit/styles/toolkit.css ../../../sites/symfony/white-space-2/web/0/css/white-space/toolkit.css
+cp build/toolkit/styles/toolkit.css.map ../../../sites/symfony/white-space-2/web/0/css/white-space/toolkit.css.map
+
 
 cp build/toolkit/scripts/toolkit.js ../../../sites/symfony/white-space-2/web/0/js/white-space/toolkit.js
 
